9ab52601936793b973aebdeae37d28e50bda5291
Scarface69420 <nixonkurian.nk@gmail.com>
2023-08-02 12:15:07 +0530

    Add project files.

---
+++ Services/EmailService/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace SimpleEmail.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("dante.roberts28@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config.GetSection("Email:Host").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("Email:UserName").Value, _config.GetSection("Email:Password").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
